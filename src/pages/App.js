ce65ab815348ff98eee4e729da4411f580fc369a
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-11-27 23:57:26 +0530

    Material UI added and header component added.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header/Header';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to React</h1>
+        <Header />
       </div>
     );
   }
